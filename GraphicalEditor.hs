0562330cee73450510c2cb0133c2eeac717e3f53
Neill Bogie <neillbogie@googlemail.com>
2012-04-09 17:04:33 +0100

    placate hlint and Wall

--- GraphicalEditor.hs
+++ GraphicalEditor.hs
@@ -1,13 +1,14 @@
 import Array
 import Control.Monad.Writer  
 import Data.Function (on)
-import Data.List (foldl', groupBy, sortBy)
+import Data.List (groupBy, sortBy)
 import Data.Maybe (mapMaybe)
 import Data.Ord (comparing)
 
-main = interact (unlines . map display . snd . runWriter . foldCmds . takeWhile ( /= Exit) . mapMaybe parse . lines) 
+main = interact $ 
+  unlines . map display . snd . runWriter . foldCmds . 
+  takeWhile ( /= Exit) . mapMaybe parse . lines
 
-type Color = Char
 data Cmd = New Int Int 
          | Clear
          | Exit
@@ -19,46 +20,47 @@ data Cmd = New Int Int
          | Save String
          deriving (Show, Eq)
 
+data Image = Image MyAr deriving (Show)
+type MyAr = Array (Int, Int) Char
+type NamedImage = (String, Image)
+type Color = Char
+white = '0'
+
 parse :: String -> Maybe Cmd
 parse = p . words
 p :: [String] -> Maybe Cmd
 p ["I",m,n]             = Just $ New (read m) (read n)
-p ["C"]                 = Just Clear
+p ["C"]                 = Just   Clear
 p ["L",x,y,[c]]         = Just $ ColorPix (read x) (read y) c
 p ["V",x,y1,y2,[c]]     = Just $ VSeg (read x) (read y1) (read y2) c
 p ["H",x1,x2,y,[c]]     = Just $ HSeg (read x1) (read x2) (read y) c
 p ["K",x1,x2,y1,y2,[c]] = Just $ Rect (read x1) (read x2) (read y1) (read y2) c
 p ["F",x,y,[c]]         = Just $ Fill (read x) (read y) c
 p ["S",name]            = Just $ Save name
-p ["X"]                 = Just $ Exit
-p other                 = Nothing
-
-type MyAr = Array (Int, Int) Char
-
-data Image = Image MyAr deriving (Show)
-type NamedImage = (String, Image)
-
+p ["X"]                 = Just   Exit
+p _                     = Nothing
 
 foldCmds :: [Cmd] -> Writer [NamedImage] Image
-foldCmds cs = foldM applyM initImage cs
+foldCmds = foldM applyM initImage
 -- TODO: don't cheat, making an image that wasn't requested.  
 -- The fold's acc should carry a Maybe Image, to represent 
 -- the possibility that it hasn't been initialised yet.
 initImage = Image $ mkArray (1,1)
 
-white = '0'
 applyM :: Image -> Cmd -> Writer [NamedImage] Image
 applyM img       (Save n)             = tell [(n,img)] >> return img
 applyM img       cmd                  = return $ apply img cmd
 
 apply :: Image -> Cmd -> Image
 apply (Image ar) Clear                = Image $ mkArray $ snd $ bounds ar
-apply img        (New w h)            = Image $ mkArray (w, h)
+apply _          (New w h)            = Image $ mkArray (w, h)
 apply (Image ar) (ColorPix x y c)     = Image $ ar // [((x,y),c)]
 apply (Image ar) (VSeg x y1 y2 c)     = Image $ ar // [((x,y),c) | y<- [y1..y2]]
 apply (Image ar) (HSeg x1 x2 y c)     = Image $ ar // [((x,y),c) | x<- [x1..x2]]
 apply (Image ar) (Rect x1 x2 y1 y2 c) = Image $ ar // [((x,y),c) | x<- [x1..x2], y<- [y1..y2]]
-apply img        Exit                 = error "Program Bug: Cmd 'Exit' applied unexpectedly!"
+apply _          Exit                 = error "Program Bug: Cmd 'Exit' applied unexpectedly!"
+-- yuck: we don't *intend* to call this apply with Save, but type allows it
+apply (Image _)  (Save _)             = error "Program Bug: Cmd 'Save' not handled at outer apply!"
 -- not implemented
 apply img        (Fill x y c)         = img
 
@@ -77,4 +79,3 @@ displayImage (Image ar) =
   sortBy (comparing y) .  -- flip assocs to row, col
   assocs $ ar
   where y = snd . fst
-
